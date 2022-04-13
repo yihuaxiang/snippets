599d95dc89d7e9f94689befd27b6d3f75970ca5f
敖武 <dongdong.fdd@alibaba-inc.com>
2022-04-13 19:40:42 +0800

    chore

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
  jekyll build
+
+
+./bin/ossutilmac64 cp ./_site oss://fudongdong-snippets  --access-key-secret=XXXX --access-key-id=XXXX -e oss-cn-beijing.aliyuncs.com --recursive --force
